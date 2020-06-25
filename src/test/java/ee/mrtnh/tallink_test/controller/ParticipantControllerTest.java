b89777e6f4dd68d231232be86ec26989d78b8593
mheinsalu <mheinsalu@gmail.com>
2020-06-25 22:24:57 +0300

    Added Controller tests

--- src/test/java/ee/mrtnh/tallink_test/controller/ParticipantControllerTest.java
+++ src/test/java/ee/mrtnh/tallink_test/controller/ParticipantControllerTest.java
@@ -2,7 +2,8 @@ package ee.mrtnh.tallink_test.controller;
 
 import ee.mrtnh.tallink_test.model.Conference;
 import ee.mrtnh.tallink_test.model.ConferenceRoom;
-import ee.mrtnh.tallink_test.service.ConferenceServiceImpl;
+import ee.mrtnh.tallink_test.model.Participant;
+import ee.mrtnh.tallink_test.service.ParticipantServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,33 +13,42 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
 
 import static org.mockito.Mockito.doReturn;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class ConferenceControllerTest {
+class ParticipantControllerTest {
 
     @MockBean
-    private ConferenceServiceImpl conferenceService;
+    private ParticipantServiceImpl participantService;
 
     @Autowired
     private MockMvc mockMvc;
 
     private Conference conference;
+    private Participant participant;
 
     private String validJson = "{\n" +
+            "    \"participant\": {\n" +
+            "        \"fullName\": \"FirstName LastName\",\n" +
+            "        \"dateOfBirth\": \"10-06-2020\"\n" +
+            "    },\n" +
+            "    \"conference\": {\n" +
             "    \"name\": \"testConferenceName\",\n" +
             "    \"startDateTime\": \"10-06-2020 10:10\",\n" +
             "    \"endDateTime\": \"10-06-2020 11:15\",\n" +
-            "    \"conferenceRoom\": {\n" +
-            "        \"name\": \"ConferenceRoom_1\",\n" +
-            "        \"location\": \"Location_1\",\n" +
-            "        \"maxSeats\": 5\n" +
+            "        \"conferenceRoom\": {\n" +
+            "            \"name\": \"ConferenceRoom_1\",\n" +
+            "            \"location\": \"Location_1\",\n" +
+            "            \"maxSeats\": 5\n" +
+            "        }\n" +
             "    }\n" +
             "}";
 
@@ -50,58 +60,42 @@ class ConferenceControllerTest {
         Integer maxCapacity = 5;
         ConferenceRoom conferenceRoom = new ConferenceRoom("testRoomName", "testRoomLocation", maxCapacity);
         conference.setConferenceRoom(conferenceRoom);
+
+        LocalDate dateOfBirth = LocalDate.of(2020, Month.JUNE, 20);
+        participant = new Participant("FirstName LastName", dateOfBirth);
     }
 
     @Test
-    void addConference_validJson() throws Exception {
-        doReturn("test message").when(conferenceService).addConference(conference);
-        // .content(asJsonString(conference))) doesn't work. Can't find method
-        mockMvc.perform(post("/addConference")
+    void addParticipantToConference_validJson() throws Exception {
+        doReturn("test message").when(participantService).addParticipantToConference( participant, conference);
+        mockMvc.perform(post("/addParticipantToConference")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(validJson))
                 .andExpect(status().isOk());
     }
 
     @Test
-    void addConference_invalidJson() throws Exception {
-        doReturn("test message").when(conferenceService).addConference(conference);
-        mockMvc.perform(post("/addConference")
+    void addParticipantToConference_invalidJson() throws Exception {
+        doReturn("test message").when(participantService).addParticipantToConference( participant, conference);
+        mockMvc.perform(post("/addParticipantToConference")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(""))
                 .andExpect(status().is4xxClientError());
     }
 
     @Test
-    void cancelConference_validJson() throws Exception {
-        doReturn("test message").when(conferenceService).cancelConference(conference);
-        mockMvc.perform(delete("/cancelConference")
+    void removeParticipantFromConference_validJson() throws Exception {
+        doReturn("test message").when(participantService).addParticipantToConference( participant, conference);
+        mockMvc.perform(delete("/removeParticipantFromConference")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(validJson))
                 .andExpect(status().isOk());
     }
 
     @Test
-    void cancelConference_invalidJson() throws Exception {
-        doReturn("test message").when(conferenceService).cancelConference(conference);
-        mockMvc.perform(delete("/cancelConference")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(""))
-                .andExpect(status().is4xxClientError());
-    }
-
-    @Test
-    void checkConferenceSeatsAvailability_validJSon() throws Exception {
-        doReturn("test message").when(conferenceService).checkConferenceSeatsAvailability(conference);
-        mockMvc.perform(get("/checkConferenceSeatsAvailability")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(validJson))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    void checkConferenceSeatsAvailability_invalidJSon() throws Exception {
-        doReturn("test message").when(conferenceService).checkConferenceSeatsAvailability(conference);
-        mockMvc.perform(get("/checkConferenceSeatsAvailability")
+    void removeParticipantFromConference_invalidJson() throws Exception {
+        doReturn("test message").when(participantService).addParticipantToConference( participant, conference);
+        mockMvc.perform(delete("/removeParticipantFromConference")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(""))
                 .andExpect(status().is4xxClientError());
